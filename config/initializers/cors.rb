b2acefa41348a1bc8cc2306f2333bdc18f206d25
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-01 20:24:48 +0900

    本番環境様にCORSのorigin設定を変更した

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins ENV['FRONT_DOMAIN'] || Rails.application.credentials.production.front[:origin]
 
     resource "*",
       headers: :any,
